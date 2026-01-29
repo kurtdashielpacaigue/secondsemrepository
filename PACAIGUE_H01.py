f2a430b08553aeadc40e26aca3111a2e6664c69d
kurtdashielpacaigue <kurtdashielpacaigue@gmail.com>
2026-01-29 23:26:52 +0800

    Update PACAIGUE_H01.py

--- PACAIGUE_H01.py
+++ PACAIGUE_H01.py
@@ -1,14 +1,14 @@
 word =input("Enter your word")
-
+word_length= len(word)
 numbers = []
-for i in range (1,7):
+for i in range (1,word_length +1):
       num = int(input(f"enter your number {i}:"))
       numbers.append(num)
 
 print(numbers)
 
-word_length= len(word)
-print(f"your word legth is {(word_length)}")
+
+print(f"your word length is {(word_length)}")
 
 avg= sum(numbers) / len(numbers)
 print(f"The average of the number is{avg}")
@@ -20,4 +20,4 @@ elif word_length <avg:
       print("The length of the word{word} is less than the average")
 
 else:
-      print("rai is gay")
+      print("What you typed in is probably an error try again")
