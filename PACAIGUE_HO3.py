c5d807dc0e04d12723d503a39834722970012b1b
kurtdashielpacaigue <kurtdashielpacaigue@gmail.com>
2026-03-13 19:39:12 +0800

    Fix label formatting and update text prompts

--- PACAIGUE_HO3.py
+++ PACAIGUE_HO3.py
@@ -1,4 +1,5 @@
 
+
 import tkinter as tk
 window = tk.Tk()
 window.title("Pacaigue_HO3")
@@ -12,7 +13,7 @@ label.grid(row=0, column=0, columnspan=2, pady=10)
 frame=tk.Frame(window,bg="blue")
 frame.grid(padx=5,pady=10,columnspan=5)
 
-labell=tk.Label(window,text="Enter 1st number")
+labell=tk.Label(window,text="Enter 1st number:")
 labell.grid(row=1, column=0, padx=5, pady=5)
 
 
@@ -23,14 +24,14 @@ entry.grid(row=1, column=1,pady=10,padx=10)
 entry1=tk.Entry(window,width=10)
 entry1.grid(row=2, column=1,pady=10,padx=10)
 
-labell=tk.Label(window,text="Enter 1st number")
+labell=tk.Label(window,text="Enter 2nd number:")
 labell.grid(row=2, column=0, padx=5, pady=5)
 
 def add():
     add1=int(entry.get())
     add2=int(entry1.get())
     add3=add1+add2
-    label["text"]=f"the sum of {add1}+{add2}is{add3}"
+    label["text"]=f"the sum of {add1} + {add2} is {add3}."
 
 button=tk.Button(window,text="add",command=add,state="normal")
 button.grid(row=3,column=0)
@@ -39,7 +40,7 @@ def subtract():
     add1=int(entry.get())
     add2=int(entry1.get())
     add3=add1-add2
-    label["text"]=f"the sum of {add1}-{add2}is{add3}"
+    label["text"]=f"the sum of {add1} - {add2} is {add3}."
 
 button=tk.Button(window,text="subtract",command=subtract,state="normal")
 button.grid(row=3,column=1,columnspan=1)
@@ -48,7 +49,7 @@ def multiply():
     add1=int(entry.get())
     add2=int(entry1.get())
     add3=add1*add2
-    label["text"]=f"the sum of {add1}*{add2}is{add3}"
+    label["text"]=f"the sum of {add1} * {add2} is {add3}."
 
 button=tk.Button(window,text="multiply",command=multiply,state="normal")
 button.grid(row=4,column=0)
@@ -58,7 +59,7 @@ def division():
     add1=int(entry.get())
     add2=int(entry1.get())
     add3=add1/add2
-    label["text"]=f"the sum of {add1}/{add2}is{add3}"
+    label["text"]=f"the sum of {add1} / {add2} is {add3}."
 
 button=tk.Button(window,text="division",command=division,state="normal")
 button.grid(row=4,column=1)
