56fb6b9c3da19df2528c6389bfc8e5632e3bb324
kurtdashielpacaigue <kurtdashielpacaigue@gmail.com>
2026-05-10 22:00:07 +0800

    Update Pacaigue_faveRecorder.py

--- Pacaigue_faveRecorder.py
+++ Pacaigue_faveRecorder.py
@@ -15,30 +15,43 @@ else:
     headers = ["ID", "First Name", "Last Name", "Birth Year", "Age"]
     ws.append(headers)
 
-firstname = input("Input Your First Name: ")
-lastname = input("Input Your Last Name: ")
-
-while True:
-    birthyear = input("Birth Year: ")
-
-    if birthyear.isdigit() == False:
-        print("You must input a valid birth year")
-
-    else:
-        break
-
-def computeage():
+def computeage(birthyear):
     age = 2026 - int(birthyear)
     return age
 
-def appentoexcel():
+def appentoexcel(firstname, lastname, birthyear):
     newid = ws.max_row
 
-    data = [ newid, firstname,lastname,int(birthyear),computeage()]
+    data = [
+        newid,
+        firstname,
+        lastname,
+        int(birthyear),
+        computeage(birthyear)
+    ]
 
     ws.append(data)
     wb.save(filename)
 
-    print("Your new info has been added successfully!")
-appentoexcel()
+    print("Your new info has been added successfully!\n")
+
+for i in range(3):
+
+    print(f"\nPerson #{i + 1}")
+
+    firstname = input("Input Your First Name: ")
+    lastname = input("Input Your Last Name: ")
+
+    while True:
+        birthyear = input("Birth Year: ")
+
+        if birthyear.isdigit() == False:
+            print("You must input a valid birth year")
+
+        else:
+            break
+
+    appentoexcel(firstname, lastname, birthyear)
+
+print("All data has been saved!")
 
